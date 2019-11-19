eed1a47319ba5eb324c25694bb6a23eaca5b9705
narumincho <narumincho.starfy@gmail.com>
2019-11-19 20:40:56 +0900

    add UserManager

--- src/clavision/UserManager.java
+++ src/clavision/UserManager.java
@@ -1,3 +1,22 @@
 package clavision;
 
-public class UserManager {}
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class UserManager {
+    static public User logInWithAccessToken(String accessToken) {
+        try {
+            final byte[] accessTokenHash = MessageDigest.getInstance("SHA-256").digest(accessToken.getBytes());
+            return UserDAO.getByAccessTokenHash(accessTokenHash);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    static public User logInWithLineCode(String  code) {
+        return UserDAO.getByLineId(code);
+    }
+
+}
