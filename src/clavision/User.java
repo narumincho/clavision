1b239b044e2d335c82758ab9af9490f05f259d01
narumincho <narumincho.starfy@gmail.com>
2019-11-19 20:40:05 +0900

    User implements Serializable

--- src/clavision/User.java
+++ src/clavision/User.java
@@ -1,6 +1,8 @@
 package clavision;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
   private String userId;
   private byte[] accessTokenHash;
   private String lineId;
