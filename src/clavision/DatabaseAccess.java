e1d426062ab0e675573dcf7f9ff8a0421f31c93a
narumincho <narumincho.starfy@gmail.com>
2019-12-03 16:56:28 +0900

    Class.forNameを削除しきれてなかった

--- src/clavision/DatabaseAccess.java
+++ src/clavision/DatabaseAccess.java
@@ -13,10 +13,9 @@ class DatabaseAccess {
   static Connection getConnection() {
     if (connection == null) {
       try {
-        Class.forName("org.postgresql.Driver");
         connection = DriverManager.getConnection(url, "postgres", Key.postgresPassword);
         return connection;
-      } catch (SQLException | ClassNotFoundException e) {
+      } catch (SQLException e) {
         e.printStackTrace();
       }
     }
