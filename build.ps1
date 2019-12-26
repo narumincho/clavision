16aceb622bbdce0874149292a121dcef9679bec7
narumincho <narumincho.starfy@gmail.com>
2019-12-26 19:19:46 +0900

    Program Filesの謎 . を削除

--- build.ps1
+++ build.ps1
@@ -1,7 +1,8 @@
-Write-Output "build to tomcat..."
+Write-Output "build to tomcat...";
 
-javac.exe ./src/clavision/Hello.java -classpath "C:\Program Files\PostgreSQL\pgJDBC\postgresql-42.2.5.jar;C:\Program Files\Apache Software Foundation\Tomcat 9.0\lib\servlet-api.jar;C:\Users\narum\.m2\repository\org\jetbrains\annotations\17.0.0\annotations-17.0.0.jar;" -d "C:/Program Files./Apache Software Foundation/Tomcat 9.0/webapps/clavision/WEB-INF/classes" -encoding utf8;
+javac.exe ./src/clavision/Hello.java -classpath "C:\Program Files\PostgreSQL\pgJDBC\postgresql-42.2.5.jar;C:\Program Files\Apache Software Foundation\Tomcat 9.0\lib\servlet-api.jar;C:\Users\narum\.m2\repository\org\jetbrains\annotations\17.0.0\annotations-17.0.0.jar;" -d "C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/clavision/WEB-INF/classes" -encoding utf8;
 
-javac.exe ./src/clavision/Api.java -classpath "C:\Program Files\PostgreSQL\pgJDBC\postgresql-42.2.5.jar;C:\Program Files\Apache Software Foundation\Tomcat 9.0\lib\servlet-api.jar;C:\Users\narum\.m2\repository\org\jetbrains\annotations\17.0.0\annotations-17.0.0.jar;" -d "C:/Program Files./Apache Software Foundation/Tomcat 9.0/webapps/clavision/WEB-INF/classes" -encoding utf8;
+javac.exe ./src/clavision/Api.java -classpath "C:\Program Files\PostgreSQL\pgJDBC\postgresql-42.2.5.jar;C:\Program Files\Apache Software Foundation\Tomcat 9.0\lib\servlet-api.jar;C:\Users\narum\.m2\repository\org\jetbrains\annotations\17.0.0\annotations-17.0.0.jar;" -d "C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/clavision/WEB-INF/classes" -encoding utf8;
+
 
 Write-Output "build to tomcat ok!"
