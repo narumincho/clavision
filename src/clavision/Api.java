2057072e957c5f00a472fe6c70281ba13a9e4c9a
narumincho <narumincho.starfy@gmail.com>
2019-12-26 21:48:04 +0900

    lineへリクエストをしてみる

--- src/clavision/Api.java
+++ src/clavision/Api.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 @WebServlet("/api/*")
 public class Api extends HttpServlet {
@@ -13,7 +15,26 @@ public class Api extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     response.setCharacterEncoding("UTF-8");
+    switch (request.getPathInfo()) {
+      case "/logInUrl":
+        getLineLogInUrl();
+    }
     response.getWriter().append("パスは").append(request.getPathInfo()).close();
+  }
+
+  private static void getLineLogInUrl() {
+    try {
+      String query =
+          URLEncoder.encode("response_type", StandardCharsets.UTF_8)
+              + "="
+              + URLEncoder.encode("code", StandardCharsets.UTF_8);
+      URL url = new URI("https", "access.line.me", "/oauth2/v2.1/authorize", query, "").toURL();
+      URLConnection urlConnection = url.openConnection();
+      Object content = urlConnection.getContent();
+      System.out.println(content);
+    } catch (URISyntaxException | IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
