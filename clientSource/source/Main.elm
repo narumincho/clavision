e16b5d38e95a9ca0e4d682d2b5d40da13d343699
narumincho <narumincho.starfy@gmail.com>
2019-12-28 21:33:39 +0900

    LINEのサーバーへの問い合わせ(WIP)

--- clientSource/source/Main.elm
+++ clientSource/source/Main.elm
@@ -1,7 +1,11 @@
 module Main exposing (Model, Msg, init, subscriptions, update, view)
 
 import Browser
-import Html exposing (..)
+import Html
+import Html.Events
+import Http
+import Json.Decode
+import Url
 
 
 main : Program () Model Msg
@@ -15,27 +19,46 @@ main =
 
 
 type Model
-    = Model
+    = Model { logInUrl : Maybe Url.Url }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( Model, Cmd.none )
+    ( Model { logInUrl = Nothing }, Cmd.none )
 
 
 type Msg
-    = Msg1
-    | Msg2
+    = RequestLineLogInUrl
+    | ResponseLineLogInUrl (Result Http.Error Url.Url)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Msg1 ->
-            ( model, Cmd.none )
+        RequestLineLogInUrl ->
+            ( model, Http.get { url = "api/lineLogInUrl", expect = Http.expectJson ResponseLineLogInUrl urlDecoder } )
 
-        Msg2 ->
-            ( model, Cmd.none )
+        ResponseLineLogInUrl result ->
+            case result of
+                Ok url ->
+                    ( Model { logInUrl = Just url }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+
+urlDecoder : Json.Decode.Decoder Url.Url
+urlDecoder =
+    Json.Decode.string
+        |> Json.Decode.andThen
+            (\string ->
+                case Url.fromString string of
+                    Just url ->
+                        Json.Decode.succeed url
+
+                    Nothing ->
+                        Json.Decode.fail "URL format error"
+            )
 
 
 subscriptions : Model -> Sub Msg
@@ -47,7 +70,10 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Document Title"
     , body =
-        [ div []
-            [ text "New Document" ]
+        [ Html.div []
+            [ Html.button
+                [ Html.Events.onClick RequestLineLogInUrl ]
+                [ Html.text "LINE でログイン" ]
+            ]
         ]
     }
--- src/clavision/Api.java
+++ src/clavision/Api.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
 
 @WebServlet("/api/*")
 public class Api extends HttpServlet {
@@ -16,25 +19,58 @@ public class Api extends HttpServlet {
       throws ServletException, IOException {
     response.setCharacterEncoding("UTF-8");
     switch (request.getPathInfo()) {
-      case "/logInUrl":
-        getLineLogInUrl();
+      case "/lineLogInUrl":
+        getLineLogInUrl(response);
+        return;
     }
-    response.getWriter().append("パスは").append(request.getPathInfo()).close();
+    response
+        .getWriter()
+        .append("error path=")
+        .append(request.getPathInfo())
+        .append("?")
+        .append(request.getQueryString())
+        .close();
   }
 
-  private static void getLineLogInUrl() {
+  private static void getLineLogInUrl(HttpServletResponse response) {
     try {
-      String query =
-          URLEncoder.encode("response_type", StandardCharsets.UTF_8)
-              + "="
-              + URLEncoder.encode("code", StandardCharsets.UTF_8);
-      URL url = new URI("https", "access.line.me", "/oauth2/v2.1/authorize", query, "").toURL();
+      URL url =
+          new URI(
+                  "https",
+                  "access.line.me",
+                  "/oauth2/v2.1/authorize",
+                  URLQueryStringFromMap(
+                      Map.of(
+                          "response_type",
+                          "code",
+                          "client_id",
+                          "1653666685",
+                          "redirect_uri",
+                          "http://localhost:8080/clavision/api/callback",
+                          "scope",
+                          "profile openid",
+                          "state",
+                          "abcd")),
+                  "")
+              .toURL();
       URLConnection urlConnection = url.openConnection();
       Object content = urlConnection.getContent();
       System.out.println(content);
+      response.getWriter().append(content.toString());
     } catch (URISyntaxException | IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private static String URLQueryStringFromMap(Map<String, String> hashMap) {
+    StringJoiner stringJoiner = new StringJoiner("&");
+    for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+      stringJoiner.add(
+          URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8)
+              + "="
+              + URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
+    }
+    return stringJoiner.toString();
   }
 
   @Override
