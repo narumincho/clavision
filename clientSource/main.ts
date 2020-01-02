9b8a61bb324ca4845821f8c35302916ae9ce2d00
narumincho <narumincho.starfy@gmail.com>
2020-01-02 11:21:01 +0900

    crate main view

--- clientSource/main.ts
+++ clientSource/main.ts
@@ -1,6 +1,17 @@
 import { Elm } from "./source/Main.elm";
+const elmAppElement = document.createElement("div");
+
+// bodyの子要素を削除
+document.documentElement.replaceChild(
+  document.body.cloneNode(false),
+  document.body
+);
+document.body.appendChild(elmAppElement);
+
+document.body.append(elmAppElement);
 const app = Elm.Main.init({
-  flags: null
+  flags: null,
+  node: elmAppElement
 });
 
 app.ports.jumpPage.subscribe(url => {
--- clientSource/source/Main.elm
+++ clientSource/source/Main.elm
@@ -1,8 +1,11 @@
 port module Main exposing (Model, Msg, init, subscriptions, update, view)
 
 import Browser
+import Css
 import Html
-import Html.Events
+import Html.Styled as S
+import Html.Styled.Attributes as A
+import Html.Styled.Events
 import Http
 import Json.Decode
 import Url
@@ -13,7 +16,7 @@ port jumpPage : String -> Cmd msg
 
 main : Program () Model Msg
 main =
-    Browser.document
+    Browser.element
         { init = init
         , view = view
         , update = update
@@ -22,32 +25,55 @@ main =
 
 
 type Model
-    = Model { result : Maybe (Result Http.Error Url.Url) }
+    = Model
+        { result : Maybe (Result Http.Error Url.Url)
+        , tab : Tab
+        }
+
+
+type Tab
+    = FloorMap
+    | TimeTable
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( Model { result = Nothing }, Cmd.none )
+    ( Model { result = Nothing, tab = FloorMap }, Cmd.none )
 
 
 type Msg
     = RequestLineLogInUrl
     | ResponseLineLogInUrl (Result Http.Error Url.Url)
+    | ChangeTab Tab
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update msg (Model record) =
     case msg of
         RequestLineLogInUrl ->
-            ( model, Http.get { url = "api/lineLogInUrl", expect = Http.expectJson ResponseLineLogInUrl urlDecoder } )
+            ( Model record
+            , Http.get
+                { url = "api/lineLogInUrl"
+                , expect = Http.expectJson ResponseLineLogInUrl urlDecoder
+                }
+            )
 
         ResponseLineLogInUrl result ->
             case result of
                 Ok url ->
-                    ( Model { result = Just result }, jumpPage (Url.toString url) )
+                    ( Model { record | result = Just result }
+                    , jumpPage (Url.toString url)
+                    )
 
                 Err _ ->
-                    ( Model { result = Just result }, Cmd.none )
+                    ( Model { record | result = Just result }
+                    , Cmd.none
+                    )
+
+        ChangeTab tab ->
+            ( Model { record | tab = tab }
+            , Cmd.none
+            )
 
 
 urlDecoder : Json.Decode.Decoder Url.Url
@@ -69,36 +95,98 @@ subscriptions model =
     Sub.none
 
 
-view : Model -> Browser.Document Msg
+view : Model -> Html.Html Msg
 view (Model record) =
-    { title = "Document Title"
-    , body =
-        [ Html.div []
-            [ Html.button
-                [ Html.Events.onClick RequestLineLogInUrl ]
-                [ Html.text "LINE でログイン" ]
+    S.div
+        [ A.css
+            [ displayGrid
+            , gridTemplateRows [ "64px", "1fr", "64px" ]
             ]
         ]
-            ++ (case record.result of
-                    Just (Ok url) ->
-                        [ Html.text (Url.toString url) ]
+        [ S.div
+            [ A.css
+                [ Css.backgroundColor themeColor
+                , Css.color (Css.rgb 255 255 255)
+                , displayGrid
+                , Css.justifyContent Css.center
+                , Css.alignItems Css.center
+                , Css.fontSize (Css.rem 1.5)
+                ]
+            ]
+            [ S.text "クラビジョン" ]
+        , S.div
+            []
+            [ S.text "中身" ]
+        , S.div
+            [ A.css [ Css.backgroundColor themeColor, displayGrid ]
+            ]
+            [ S.div
+                [ A.css [ displayGrid, gridTemplateColumns [ "1fr", "1fr" ] ] ]
+                [ menuItem
+                    (if record.tab == TimeTable then
+                        Just (ChangeTab FloorMap)
 
-                    Just (Err (Http.BadUrl url)) ->
-                        [ Html.text ("urlが悪かった" ++ url) ]
+                     else
+                        Nothing
+                    )
+                    "フロアマップ"
+                , menuItem
+                    (if record.tab == FloorMap then
+                        Just (ChangeTab TimeTable)
 
-                    Just (Err Http.Timeout) ->
-                        [ Html.text "タイムアウトした" ]
+                     else
+                        Nothing
+                    )
+                    "時間割表"
+                ]
+            ]
+        ]
+        |> S.toUnstyled
 
-                    Just (Err Http.NetworkError) ->
-                        [ Html.text "ネットワークでエラーが発生しました" ]
 
-                    Just (Err (Http.BadStatus number)) ->
-                        [ Html.text ("エラーが発生しました" ++ String.fromInt number) ]
+menuItem : Maybe message -> String -> S.Html message
+menuItem messageMaybe text =
+    S.div
+        ([ A.css
+            [ displayGrid
+            , Css.justifyContent Css.center
+            , Css.alignItems Css.center
+            , Css.color
+                (case messageMaybe of
+                    Just _ ->
+                        Css.rgb 150 150 150
 
-                    Just (Err (Http.BadBody message)) ->
-                        [ Html.text ("レスポンスのデコードでエラーが発生しました " ++ message) ]
+                    Nothing ->
+                        Css.rgb 255 255 255
+                )
+            ]
+         ]
+            ++ (case messageMaybe of
+                    Just message ->
+                        [ Html.Styled.Events.onClick message ]
 
                     Nothing ->
                         []
                )
-    }
+        )
+        [ S.text text ]
+
+
+themeColor : Css.Color
+themeColor =
+    Css.rgb 144 62 166
+
+
+displayGrid : Css.Style
+displayGrid =
+    Css.property "display" "grid"
+
+
+gridTemplateRows : List String -> Css.Style
+gridTemplateRows list =
+    Css.property "grid-template-rows" (list |> String.join " ")
+
+
+gridTemplateColumns : List String -> Css.Style
+gridTemplateColumns list =
+    Css.property "grid-template-columns" (list |> String.join " ")
